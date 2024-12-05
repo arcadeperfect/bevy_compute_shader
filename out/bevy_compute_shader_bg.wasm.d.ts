2e58e5a0833c4352a222d2373088014d9dc02556
arcadeperfect <alexander.harding@gmail.com>
2024-12-05 00:52:33 +0000

    Deploying to gh-pages from @ arcadeperfect/bevy_compute_shader@c5b121804f4ba03ad8d5978329bc92d208d07352 🚀

---
+++ out/bevy_compute_shader_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure1518_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hbaef006a82d96477: (a: number, b: number, c: number) => void;
+export const closure4290_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h41a9f684830410d2: (a: number, b: number) => void;
+export const closure4297_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3259ebe63bec4605: (a: number, b: number) => void;
+export const closure92584_externref_shim: (a: number, b: number, c: any) => void;
+export const closure100496_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
